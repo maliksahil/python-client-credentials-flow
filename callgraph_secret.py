0071044a378276acd1c9404842fbb4d3dab6eeb8
Sahil Malik <sahilmalik@winsmarts.com>
2021-01-08 22:42:02 -0500

    Added sample to use certificate based CC flow

--- callgraph_secret.py
+++ callgraph_secret.py
@@ -1,16 +1,17 @@
 import requests, json
 
-token_url = "https://login.microsoftonline.com/sahilmalikgmail.onmicrosoft.com/oauth2/v2.0/token"
-test_api_url = "api_url"
-
 client_id = 'clientid'
 client_secret = 'clientsecret'
-scope = 'scope'
+tenantname = 'tenantname'
+
+graph_api_url = "https://graph.microsoft.com/v1.0/users"
+token_endpoint = "https://login.microsoftonline.com/" + tenantname + "/oauth2/v2.0/token"
+scope = 'https://graph.microsoft.com/.default'
 
 data = "client_id=" + client_id + "&scope=" + scope + "&client_secret=" + client_secret + "&grant_type=client_credentials"
 
 access_token_response = requests.post(
-    token_url, 
+    token_endpoint, 
     data=data, 
     verify=False, 
     allow_redirects=False, auth=(client_id, client_secret))
@@ -23,6 +24,6 @@ tokens = json.loads(access_token_response.text)
 print ("access token: " + tokens['access_token'])
 
 api_call_headers = {'Authorization': 'Bearer ' + tokens['access_token']}
-api_call_response = requests.get(test_api_url, headers=api_call_headers, verify=False)
+api_call_response = requests.get(graph_api_url, headers=api_call_headers, verify=False)
 
 print (api_call_response.text)
